2b51f651beba610e19fc6dba1d5706c20609caf8
Azie88 <obandoandrew8@gmail.com>
2023-10-15 21:31:42 +0300

    Streamlit web app

--- src/app.py
+++ src/app.py
@@ -1,72 +1,107 @@
-# Import necessary libraries
-import streamlit as st
 import pandas as pd
+import streamlit as st
+import numpy as np
+import sklearn
+import joblib
 
-# Define a function to load data
-@st.cache_resource()
-def load_data(relative_path):
-    data = pd.read_csv(r"data\split_1.csv") 
-    return data
 
-# Set the relative path to the csv and load data with function
-rel_path = r"data\split_1.csv"
-loaded_df = load_data(rel_path)
+num_imputer = joblib.load('toolkit/numerical_imputer.joblib')
+cat_imputer = joblib.load('toolkit/categorical_imputer.joblib')
+encoder = joblib.load('toolkit/encoder.joblib')
+scaler = joblib.load('toolkit/scaler.joblib')
+dt_model = joblib.load('toolkit/Final_model.joblib')
 
-# Set up the Streamlit sidebar for navigation
-st.sidebar.header('Navigation')
-menu = ['Home', 'About the App']
-choice = st.sidebar.selectbox("Select an option", menu)
+# Add a title and subtitle
+st.write("<center><h1>Sales Prediction App</h1></center>", unsafe_allow_html=True)
 
-# Set the title for the app
-st.title("Store sales prediction")
+# Load the image
+# image = Image.open("grocery_shopping_woman.png")
 
-# Create a container for the header section
-header = st.container()
-header.write("This app predicts the total sales of items sold at different stores")
+# Set up the layout
+col1, col2, col3 = st.columns([1, 3, 3])
+# col2.image(image, width=600)
 
-# Create a container for previewing the dataset
-dataset = st.container()
-with dataset:
-    if dataset.button("Preview the Dataset"):
-        dataset.write(loaded_df)
+st.write("This app uses machine learning to predict sales based on certain input parameters. Simply enter the required information and click 'Predict' to get a sales prediction!")
 
-# Create a form for user inputs
-# form = st.form(key='Information', clear_on_submit=True)
+st.subheader("Enter the details to predict sales")
 
-# Create a container for user inputs and predictions
-predictions = st.container()
-with predictions:
-    predictions.subheader("Inputs")
-    predictions.write("This section will take all your inputs")
-
-    # Define columns for user input fields
-    col1, col2 = predictions.columns(2)
-
-    Store_type = ["A", "B", "C", "D", "E"]
-    clusters = ["1", "2", "3", "4", "5","6", "7", "8", "9", "10", "11","12", "13", "14", "15", "15","17"]
-    categories = ['AUTOMOTIVE', 'BEAUTY AND FASHION', 'BEVERAGES AND LIQUOR',
+# Create the input fields
+input_data = {}
+col1,col2 = st.columns(2)
+with col1:
+    input_data['store_nbr'] = st.slider("enter store number",0,54)
+    input_data['family'] = st.selectbox("choose product family", ['AUTOMOTIVE', 'BEAUTY AND FASHION', 'BEVERAGES AND LIQUOR',
        'SCHOOL AND OFFICE SUPPLIES', 'GROCERY', 'HOME CARE AND GARDEN',
-       'FROZEN FOODS', 'HOME AND KITCHEN', 'PET SUPPLIES']
-    events = ['1', '0']
+       'FROZEN FOODS', 'HOME AND KITCHEN', 'PET SUPPLIES'])
+    input_data['onpromotion'] =st.number_input("number of items on promotion",step=1)
+    input_data['oil_price'] = st.slider("oil price",min_value=0.0, max_value=100.0, step=0.1)
+    input_data['store_type'] = st.selectbox("store_type",['D', 'C', 'B', 'E', 'A'])
+    input_data['store_cluster'] = st.slider("store cluster",0,17)
 
-    # Section 1: User inputs for date and store-related information
-    with col1:
-        col1.write('Section 1')
-        start_date = st.date_input("Start Date")
-        start_date = pd.to_datetime(start_date)
-        end_date = st.date_input("End Date")
-        end_date = pd.to_datetime(end_date)
-        onpromotion = st.number_input("How many products are on promotion?", min_value=0, step=1)
-        store_nbr = st.number_input("What is the store number", min_value=0, max_value=25)
+with col2:
+    input_data['events'] = st.slider("Is it a holiday? 0=no, 1=yes",0,1)
+    input_data['Year'] = st.number_input("year",step=1)
+    input_data['Month'] = st.slider("month",1,12)
+    input_data['Day'] = st.slider("day",1,31)
+    input_data['Quarter'] = st.slider("quarter",1,4)
+    input_data['Week_of_Year'] = st.slider("week of the year",1,53)
+    input_data['Day_of_Week'] = st.number_input("dayofweek,0=Sun and 6=Sat",step=1)
+    input_data['Is_Weekend'] = st.number_input("Is it a weekend? 0=No, 1=Yes",0,1)
 
-    # Section 2: User inputs for event and product-related information
-    with col2:
-        col2.write('Section 2')
-        events = st.selectbox("Is it a holiday? (1)Yes or (0)No", events)
-        selected_category = st.selectbox("Product_Category", categories)
-        selected_store = st.selectbox("Store_type", Store_type)
-        selected_cluster = st.selectbox("Cluster", clusters)
+# Define the custom CSS
+predict_button_css = """
+    <style>
+    .predict-button {
+        background-color: #C4C4C4;
+        color: gray;
+        padding: 0.75rem 2rem;
+        border-radius: 0.5rem;
+        border: none;
+        font-size: 1.1rem;
+        font-weight: bold;
+        text-align: center;
+        margin-top: 2rem;
+    }
+    </style>
+"""
 
-    # Create an empty DataFrame for predicted data
-    predicted_data = pd.DataFrame(columns=['Start Date', 'End Date', 'Store', 'Category', 'On Promotion', 'City', 'Cluster', 'Predicted Sales'])
+# Display the custom CSS
+st.markdown(predict_button_css, unsafe_allow_html=True)
 
+
+  # Create a button to make a prediction
+
+if st.button("Predict", key="predict_button", help="Click to make a prediction."):
+    # Convert the input data to a pandas DataFrame
+        input_df = pd.DataFrame([input_data])
+
+
+# Selecting categorical and numerical columns separately
+        cat_columns = [col for col in input_df.columns if input_df[col].dtype == 'object']
+        num_columns = [col for col in input_df.columns if input_df[col].dtype != 'object']
+
+
+# Apply the imputers
+        input_df_imputed_cat = cat_imputer.transform(input_df[cat_columns])
+        input_df_imputed_num = num_imputer.transform(input_df[num_columns])
+
+
+ # Encode the categorical columns
+        input_encoded_df = pd.DataFrame(encoder.transform(input_df_imputed_cat).toarray(),
+                                   columns=encoder.get_feature_names_out(cat_columns))
+
+# Scale the numerical columns
+        input_df_scaled = scaler.transform(input_df_imputed_num)
+        input_scaled_df = pd.DataFrame(input_df_scaled , columns = num_columns)
+
+#joining the cat encoded and num scaled
+        final_df = pd.concat([input_scaled_df, input_encoded_df], axis=1)
+
+# Make a prediction
+        prediction = dt_model.predict(final_df)[0]
+
+
+# Display the prediction
+        st.write(f"The predicted sales are: {prediction}.")
+        input_df.to_csv("data/input_data.csv", index=False)
+        st.table(input_df)
